845413484d45f7874e7361ac407b402aadfba021
Hung150 <55206412+Hung150@users.noreply.github.com>
2020-02-12 21:09:09 +0300

    Delete task1.cpp

--- task1.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int main()
-{
-	std::cout<<"Than em vua trang lai vua tron\n Bay noi ba chim voi nuoc non\n Ran nat mac dau tay ke nan\n Ma em van giu tam long son";
-	return 0;
-}
